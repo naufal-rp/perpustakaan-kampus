950fb0225587db88acde71b7ee3888c4dd1b05a5
naufal-rp <naufalputra1509@gmail.com>
2025-06-19 16:54:19 +0700

    add : qstash workflows

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import ratelimit from "../ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
   const { email, password } = params;
@@ -54,6 +56,14 @@ export const signUp = async (params: AuthCredentials) => {
       kampusId,
       password: hashedPassword,
       kampusCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: {
+        email,
+        fullName
+      }
     });
 
     await signInWithCredentials({ email, password });
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,22 @@
 import { Client as WorkFlowClient } from "@upstash/workflow";
+import { Client } from "@upstash/qstash";
 import config from "@/lib/config";
 
 export const workflowClient = new WorkFlowClient({
   baseUrl: config.env.upstash.qstashUrl,
-  token: config.env.upstash.qstashToken
+  token: config.env.upstash.qstashToken,
 });
+
+const client = new Client({ token: config.env.upstash.qstashToken });
+
+export const sendEmail = async ({ email, subject, message }: { email: string; subject: string; message: string }) => {
+  await client.publishJSON({
+    url: `${config.env.prodApiEndpoint}/api/send-email`,
+    body: {
+      from: "Permpus <hello.permpus.com>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
